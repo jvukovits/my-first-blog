80c3d5771ecd1a9d3ef23fb1537c90c33dc57be7
Jeff Vukovits <jeffvukovits@icloud.com>
2026-02-28 11:35:11 -0500

    Updating Python Anywhere

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
+from blog import admin
 from . import views
+from django.urls import path, include
+from django.contrib import admin
+
+from django.contrib.auth import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('', include('blog.urls')),
 ]
